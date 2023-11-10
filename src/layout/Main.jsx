15733cad19ceeeea1a60b743a034ee0ea1713a6c
Zhibek Zhagypar <zhibek.zhagypar@Zhibeks-MacBook-Air.local>
2023-11-10 21:30:02 +0600

    Fixed the link to api. Added catch function

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,20 +12,28 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch("http://www.omdbapi.com/?apikey=${API_KEY}&s=attack")
+    fetch("https://www.omdbapi.com/?apikey=${API_KEY}&s=attack")
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`,
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
